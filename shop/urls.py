6fd60f9364cde250a19305e37f4f3e5c6fd86353
Gautam singh <gautamk1512>
2023-09-20 19:45:37 +0530

    update app

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="ShopHome"),
-    path('about/',  views.about, name="AuoutUS"),
+        
     path('Contact/', views.contact, name="contactUS"),
     path('tracker/', views.tracker, name="TrackingStatus"),
     path('search/', views.search, name="Search"),
